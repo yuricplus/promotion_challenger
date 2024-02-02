cf7f0ad1bea2908b37b1dd92b80c8db657c3fb9b
Yuri Martins <yurigamer16@gmail.com>
2024-02-02 01:41:22 -0300

    feat: create pagination

---
+++ src/app/shared/components/pagination/pagination.tsx
@@ -0,0 +1,59 @@
+import './style.scss'
+interface IPropsPagination {
+	handlePageChange: Function,
+	totalPages: number,
+	actualPage: number
+}
+
+interface IPage {
+	page: number,
+}
+export const PaginationComponent = (props: IPropsPagination) => {
+	let startFrom = 1;
+
+	if(props.totalPages <= 1) return null;
+
+	const limitPagination = ():number => {
+		const maxItem = 10;
+		let page = 0;
+
+		if(props.totalPages < maxItem) return props.totalPages;
+
+		if(props.actualPage >= maxItem) {
+			startFrom = props.actualPage - 10;
+			page = maxItem + startFrom + 1;
+
+			console.log(page)
+
+			console.log(startFrom)
+			return page;
+		}
+
+		console.log('badah')
+		return maxItem;
+	}
+
+
+	const ButtonComponent = (btnProps: IPage) => {
+		if(btnProps.page <= startFrom) return null;
+		return (
+			<li className='content-item'>
+				<button
+					onClick={() => props.handlePageChange(btnProps.page)}
+					aria-label={`Pagina ${btnProps.page}`}
+					className={btnProps.page === props.actualPage ? 'active' : ''}>
+					{btnProps.page}
+				</button>
+				</li>
+		)
+	}
+
+	
+	return (
+		<ul className="container-pagination">
+			{Array.from(Array(limitPagination()), (_, i) => i + 1).map((page: number) => (
+				<ButtonComponent page={page} key={page}/>
+			))}
+		</ul>
+	)
+}
