8aa6c0bfecd380e1b2eb5821024c693d333d154f
Yuri Martins <yurigamer16@gmail.com>
2024-02-02 01:41:08 -0300

    feat: create logo redirect

--- src/app/shared/components/header/index.tsx
+++ src/app/shared/components/header/index.tsx
@@ -1,3 +1,4 @@
+import Link from 'next/link';
 import { FormSearch } from '..';
 
 import './style.scss';
@@ -11,6 +12,11 @@ interface IHeaderComponentProps {
 const HeaderComponent = (props: IHeaderComponentProps) => {
     return (
         <header className="container-header">
+            <div className="content-logo">
+                <Link href={'/'}>
+                  <span className='logo' role='img'>MKT</span>
+                </Link>
+            </div>
             <div className="content-search">
                 <FormSearch searchProducts={props.callBackSearch}/>
             </div>
