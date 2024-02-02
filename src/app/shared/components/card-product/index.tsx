98596aac0770af6a6df98506e2d1816e5eec6b0c
Yuri Martins <yurigamer16@gmail.com>
2024-02-02 01:40:55 -0300

    feat: card product

--- src/app/shared/components/card-product/index.tsx
+++ src/app/shared/components/card-product/index.tsx
@@ -1,6 +1,7 @@
 import Image from "next/image";
 import './style.scss'
 import Link from "next/link";
+import { formatPrice } from "../../services/currency-format/currency-format.service";
 
 interface ICardProductComponentProps {
     url: string,
@@ -11,24 +12,24 @@ interface ICardProductComponentProps {
 
 const CardProduct = (props: ICardProductComponentProps) => {
     return (
-        <Link href={`details?id=${props.id}`} tabIndex={-1}>
+        <Link href={`details/${props.id}`} tabIndex={-1}>
             <div className="card">
-            <div className="card-image">
-                <Image 
-                    src={props.url}
-                    alt={props.title}
-                    width={100}
-                    height={100}
-                    aria-hidden
-                />
+                <div className="card-image">
+                    <Image
+                        src={props.url}
+                        alt={props.title}
+                        width={100}
+                        height={100}
+                        aria-hidden
+                    />
+                </div>
+                <div className="card-title" tabIndex={0} aria-level={2}>
+                    <span className="title-product">{props.title}</span>
+                </div>
+                <div className="card-price" aria-label={props.price} aria-roledescription="Preço">
+                    <span className="price-product">{formatPrice(props.price)}</span>
+                </div>
             </div>
-            <div className="card-title" tabIndex={0} aria-level={2}>
-                <span className="title-product">{props.title}</span>
-            </div>
-            <div className="card-price" aria-label={props.price} aria-roledescription="Preço">
-                <span className="price-product">R$ {props.price}</span>
-            </div>
-        </div>
         </Link>
     )
 }
