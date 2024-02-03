8ae835e5e4c8b944bc4274eefe74778f4252177a
Yuri Martins <yurigamer16@gmail.com>
2024-02-03 01:51:55 -0300

    fix: search by offset

--- src/app/search/page.tsx
+++ src/app/search/page.tsx
@@ -9,31 +9,52 @@ import { IProductData, ISearcResult } from "../shared/interfaces/search-result.i
 import './style.scss'
 import { useRouter } from "next/navigation";
 import { PaginationComponent } from "../shared/components/pagination/pagination";
+import { LoadingComponent } from "../shared/components/loading/loading.component";
+import { SearchService } from "../shared/services/search/search.service";
 
 export default function search() {
     const searchParams = useSearchParams();
     const [result, setResult] = useState<IProductData[]>([])
-    const [keyWords, setKeyWords] = useState<string>()
+    const [keyWords, setKeyWords] = useState<string>();
+    const [loading, setLoading] = useState(false);
+    const [actualPage, setActualPage] = useState(1)
+    const [limit, setLimits] = useState(0)
     const router = useRouter()
 
     const handelPage = (page: number) => {
-        console.log(page)
+        const terms = searchParams.get('term') as string;
+        scrolToTop();
+        setActualPage(page);
+        getProductsByTerms(terms)
     }
 
     const searchProducts = (e: Event) => {
         e.preventDefault();
         const form = e.target as HTMLFormElement;
         const search = form.querySelector('#search') as HTMLInputElement;
-        if(search.value) {
+        if (search.value) {
             router.push(`/search?term=${search.value}`)
-           getProductsByTerms(search.value)
+            getProductsByTerms(search.value)
         }
-    } 
+    }
     const getProductsByTerms = async (terms: string) => {
-        const baseService = new HttpBaseService();
-        const response = await baseService.get(`sites/MLB/search?q=${terms}`) as ISearcResult;
-        setResult(response?.results);
-        setKeyWords(response.query)
+        const search = document.querySelector('#search') as HTMLInputElement;
+        setLoading(true)
+        try {
+            const searchService = new SearchService();
+            const response = await searchService.searchProducts(terms, actualPage) as ISearcResult;
+            setLimits(response.paging.limit);
+            setResult(response?.results);
+            setKeyWords(terms);
+            search.value = terms;
+            setLoading(false)
+        } catch (error) {
+            setLoading(false)
+        }
+    }
+
+    const scrolToTop = () => {
+        window.scrollTo({top: 0, behavior: 'smooth'})
     }
 
     useEffect(() => {
@@ -41,22 +62,29 @@ export default function search() {
         getProductsByTerms(terms)
     }, [])
 
+    if(loading) return (
+        <>
+         <HeaderComponent callBackSearch={searchProducts}></HeaderComponent>
+         <LoadingComponent/>
+        </>
+      );
+
     return (
-       <>
-        <HeaderComponent callBackSearch={searchProducts}></HeaderComponent>
-        <section className="container">
-            <div className="result-keywords">
-                <h1>Resultado: {keyWords}</h1>
-            </div>
-            <div className="products-result">
-                {result.map((item) => (
-                    <CardProduct url={item.thumbnail} title={item.title} price={item.price} id={item?.id} key={item?.id}/>
-                ))}
-            </div>
-            <div className="bottom-result">
-                <PaginationComponent totalPages={20} handlePageChange={(e: number) => handelPage(e)} actualPage={14}/>
-            </div>
-        </section>
-       </>
+        <>
+            <HeaderComponent callBackSearch={searchProducts}></HeaderComponent>
+            <section className="container">
+                <div className="result-keywords">
+                    <h1>Resultado: {keyWords}</h1>
+                </div>
+                <div className="products-result">
+                    {result.map((item) => (
+                        <CardProduct url={item.thumbnail} title={item.title} price={item.price} id={item?.id} key={item?.id} />
+                    ))}
+                </div>
+                <div className="bottom-result">
+                    <PaginationComponent totalPages={limit} handlePageChange={(e: number) => handelPage(e)} actualPage={actualPage} />
+                </div>
+            </section>
+        </>
     )
 }
