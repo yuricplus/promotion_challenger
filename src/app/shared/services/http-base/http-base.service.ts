c0430ee1267d8603db37f570ebcf9212fb806774
Yuri Martins <yurigamer16@gmail.com>
2024-02-03 01:53:27 -0300

    fix: service extends

--- src/app/shared/services/http-base/http-base.service.ts
+++ src/app/shared/services/http-base/http-base.service.ts
@@ -5,29 +5,9 @@ export class HttpBaseService {
   public axios = axios.create({
     baseURL: process.env.URL_API
   });
-//   public headers = { 
-//     headers: {
-//         Authorization: `Bearer ${process.env.ACCESS_TOKEN}`
-//     }
-//   }
 
   async get(url: string, params?: any): Promise<any> {
     const response = await this.axios.get(`${url}`, { params });
-    return response.data;
-  }
-
-  async post(url: string, data: any): Promise<any> {
-    const response = await this.axios.post(url, data);
-    return response.data;
-  }
-
-  async put(url: string, data: any): Promise<any> {
-    const response = await this.axios.put(url, data);
-    return response.data;
-  }
-
-  async delete(url: string): Promise<any> {
-    const response = await this.axios.delete(url);
     return response.data;
   }
 }
--- src/app/shared/services/search/search.service.ts
+++ src/app/shared/services/search/search.service.ts
@@ -1,7 +1,25 @@
+import { IDescription, IProductDetails } from "../../interfaces/product-details.interface";
+import { IProductData } from "../../interfaces/search-result.interface";
 import { HttpBaseService } from "../http-base/http-base.service";
 
+interface IResult {
+    results: IProductData[]
+}
+
 export class SearchService extends HttpBaseService {
-    searchProducts(){
-        //this.get()
+    async searchProducts(term: string, page: number, limit?: number){
+       return await this.get(`sites/MLB/search?q=${term}?offset=${page}?limit=${limit}`)
+    }
+
+    async getProductsDetails(id: string): Promise<IProductDetails>{
+        return await this.get(`items/${id}`);
+    }
+
+    async getDescription(id: string): Promise<IDescription>{
+        return await this.get(`items/${id}/description`);
+    }
+
+    async searchCategory(): Promise<IResult>{
+        return await this.get(`sites/MLB/search?category=MLB1743`);
     }
 }
