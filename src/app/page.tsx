8a4561fabda0e8189142abb02d76ab8595cce305
Yuri Martins <yurigamer16@gmail.com>
2024-02-03 01:51:27 -0300

    feat: create home recommendation

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,17 +1,30 @@
 'use client'
 import { HeaderComponent } from "./shared";
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import { HttpBaseService } from "./shared/services/http-base/http-base.service";
 import { useRouter } from "next/navigation";
+import { IProductData } from "./shared/interfaces/search-result.interface";
+import { SearchService } from "./shared/services/search/search.service";
+import CardProduct from "./shared/components/card-product";
 
 export default function Home() {
-  const router = useRouter()
-  
+  const router = useRouter();
+  const [recommendation, setRecomendatton] = useState<IProductData[]>([]);
+
+  const getRecomendation = async () => {
+    try {
+      const searchService = new SearchService();
+      const data = await searchService.searchCategory();
+
+      setRecomendatton(data.results.slice(0, 4))
+    } catch (error) {
+
+    }
+  }
+
   useEffect(() => {
-    const baseService = new HttpBaseService();
-    baseService.get('products/search?status=active&site_id=MLA&q=Samsung 20 Galaxy S8 64 GB rosa')
-    console.log('data', process.env.URL_API)
-  }, []) 
+    getRecomendation()
+  }, [])
 
   const redirectToSearchPage = (terms: string) => {
     router.push(`/search?term=${terms}`)
@@ -21,7 +34,7 @@ export default function Home() {
     e.preventDefault();
     const form = e.target as HTMLFormElement;
     const search = form.querySelector('#search') as HTMLInputElement;
-    if(search.value) {
+    if (search.value) {
       redirectToSearchPage(search.value)
     }
   }
@@ -29,8 +42,15 @@ export default function Home() {
   return (
     <>
       <HeaderComponent callBackSearch={searchProducts}></HeaderComponent>
-      <main>
-     </main>
+      <main className="main-section">
+        <div className="banner-cta"></div>
+        <div className="divider-shadow"></div>
+        <div className="container-product">
+          {recommendation.map((item) => (
+            <CardProduct url={item.thumbnail} title={item.title} price={item.price} id={item?.id} key={item?.id} />
+          ))}
+        </div>
+      </main>
     </>
   );
 }
